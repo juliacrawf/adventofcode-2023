e16bfa24684d051816b36d81d3bd20815ecf65c7
Julia Crawford <juliacraw0@gmail.com>
2023-12-02 18:20:36 -0800

    Add day1, part1 solution

---
+++ day1/day1.py
@@ -0,0 +1,41 @@
+
+# Read the file input
+fr = open("day1input.txt", "r")
+#fr = open("day1inputexample.txt", "r")
+lines = (fr.read()).splitlines()
+
+# Initialize numbers list and other vars
+vals=[None]*len(lines)
+
+bigSum=0
+i=0
+x=0
+
+# Loop through every line, then every character on every line and extract numbers
+for line in lines:
+    chars = [x for x in line]
+
+    for char in chars:
+        if not char.isalpha():
+            if vals[i] == None:
+                vals[i]=char
+            else:
+                vals[i]=vals[i]+char
+    i=i+1
+
+# Fix all the values to be first and last digit (or duplicate the first)
+j=0
+for val in vals:
+    if len(val) < 2:
+        vals[j]=val[0]+val[0]
+    if len(val) > 2:
+        vals[j]=val[0]+val[len(val)-1]
+    j=j+1
+
+
+# Sum up all of the numbers
+for number in vals:
+    bigSum = bigSum + int(number)
+print(bigSum)
+
+
