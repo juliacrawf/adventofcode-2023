6cf3d13159bd1c53cc84dff11bac4cc0e41d21e6
Julia Crawford <juliacraw0@gmail.com>
2023-12-12 19:15:27 -0800

    Changes for part 2

--- day6/day6.py
+++ day6/day6.py
@@ -1,15 +1,20 @@
 
 def main():
 
+    # Time: 7 15 30
+    # Distance: 9 40 200
+    # race_time = [7, 15, 30]
+    # best_distance = [9, 40, 200]
+    #race_time = [71530]
+    #best_distance = [940200]
+
     # Time: 54 81 70 88
     # Distance: 446 1292 1035 1007
-    race_time = [54, 81, 70, 88]
-    best_distance = [446, 1292, 1035, 1007]
+    # race_time = [54, 81, 70, 88]
+    # best_distance = [446, 1292, 1035, 1007]
+    race_time = [54817088]
+    best_distance = [446129210351007]
 
-    #Time: 7 15 30
-    #Distance: 9 40 200
-    #race_time = [7, 15, 30]
-    #best_distance = [9, 40, 200]
 
     i=0
     wins=[]
@@ -22,24 +27,18 @@ def main():
             if hold_time * (int(time) - hold_time) > best_distance[i]:
                 number_of_wins = number_of_wins + 1
             hold_time = hold_time + 1
-        wins.append(number_of_wins)
+        print("For the Time=" + str(time) + " and Distance=" + str(best_distance[i]) + " there are " + str(number_of_wins) + " ways to win.")
+        #wins.append(number_of_wins)
         i=i+1
 
-    if len(wins) > 0:
-        for win in wins:
-            final_product = win * final_product
-
-    print("Final product: " + str(final_product))
-
-
-
-
-
-
-
+    # Comment out for part 2
+    # if len(wins) > 0:
+    #     for win in wins:
+    #         final_product = win * final_product
+    #
+    # print("Final product: " + str(final_product))
 
 
 # Required python variable for the silly thing to run
 if __name__ == "__main__":
-    import pandas as pd
     main()
